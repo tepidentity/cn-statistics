e09fff06cdb52550b8c7a7451740fbc7d63226cf
Boyan Pavlov <tepidentity@gmail.com>
2022-06-21 18:10:48 +0300

    Adds project skeleton

---
+++ data-scrapper/src/main/java/org/example/data/scrapper/configuration/ApplicationConfiguration.java
@@ -0,0 +1,17 @@
+package org.example.data.scrapper.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+
+        return new RestTemplate();
+    }
+}
