3d10b917b0a9b0fc688c026253782ca9620750b9
Boyan Pavlov <tepidentity@gmail.com>
2022-06-24 07:53:36 +0300

    Adds documentation

--- statistics/src/main/java/org/example/statistics/client/StatisticsClient.java
+++ statistics/src/main/java/org/example/statistics/client/StatisticsClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "data-source")
+@FeignClient(value = "data-source-service")
 public interface StatisticsClient {
 
     @RequestMapping(value = "/country/{code}", method = RequestMethod.GET)
